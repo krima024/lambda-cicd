2b73621a7beaf1cf38201022a528a037a75c247d
krima024 <99860803+krima024@users.noreply.github.com>
2022-05-23 12:47:11 +0530

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,21 +1,30 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!.........')
+    }
+  
 # def lambda_handler(event, context):
 #    message = 'Hello {} !'.format(event['key1'])
 #    return {
 #        'message' : message
 #    }
-import json
-def lambda_handler(event, context):
+# import json
+# def lambda_handler(event, context):
 
-#Path parameter passed to Lambda function
- servername = event["params"]["path"]["servername"]
+# #Path parameter passed to Lambda function
+#  servername = event["params"]["path"]["servername"]
 
-#Query string parameter passed to Lambda function
+# #Query string parameter passed to Lambda function
 
- Email_id = event['params']['querystring']['emailid']
- return {
+#  Email_id = event['params']['querystring']['emailid']
+#  return {
 
-"Path Parameter" : servername,
+# "Path Parameter" : servername,
 
-"Query string Parameter" : Email_id
+# "Query string Parameter" : Email_id
 
-}
+# }
