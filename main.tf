51c5156f412436e30605a45025b2733fa61f5851
krima024 <99860803+krima024@users.noreply.github.com>
2022-05-23 12:22:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,22 +59,22 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
 type        = "zip"
-source_dir  = "/"
-output_path = "hello.zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/hello.zip"
 }
 
 #aws lambda fuction
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-# filename                       = "${path.module}/python/hello.zip"
-# function_name                  = "Lambda_Function"
-# role                           = aws_iam_role.lambda_role.arn
-# handler                        = "index.lambda_handler"
-# runtime                        = "python3.8"
-filename      = "hello.zip"
-function_name = "lambda_function"
-role          = aws_iam_role.lambda_role.arn
-handler       = "index.lambda_handler"
-runtime       = "python3.8"
+filename                       = "${path.module}/python/hello.zip"
+function_name                  = "Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+# filename      = "${path.module}/python/lambda_function.zip"
+# function_name = "lambda_function"
+# role          = aws_iam_role.lambda_role.arn
+# handler       = "index.test"
+# runtime       = "python3.8"
 depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
